6c8d56c3fc7b81b43dec910a5700ad3869f04471
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2022-06-12 22:58:07 -0400

    fix broken imports

--- custom_components/jvc_projectors/remote.py
+++ custom_components/jvc_projectors/remote.py
@@ -14,11 +14,7 @@ from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
 from .const import (
-    GAMING_MODE_HDR_COMMAND,
-    GAMING_MODE_SDR_COMMAND,
-    HDR_MODE_COMMAND,
     INFO_COMMAND,
-    SDR_MODE_COMMAND,
 )
 
 _LOGGER = logging.getLogger(__name__)
